c0640ac5589cc96e656f0da4092dd513228eb48e
Connor Allen <m.connor.allen@gmail.com>
2022-07-31 14:32:57 -0400

    deploying to heroku

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const sequelize = require('../config/connection');
 
 class User extends Model {
     checkPassword(loginPW) {
-        return bcrypt.compare(loginPW, this.password);
+        return bcrypt.compareSync(loginPW, this.password);
     }
 }
 
