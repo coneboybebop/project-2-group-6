150a16cc292fed843fde7197b5427b78b1ff6b93
joe-white09 <joe_white09@yahoo.com>
2022-07-31 21:20:46 -0500

    updated changes

--- controllers/regional-routes.js
+++ controllers/regional-routes.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const {Post, User, Zipcode } = require('../models');
 
-<<<<<<< HEAD
 router.get('/:id', (req, res) => {
     Post.findAll({
         where: {
@@ -27,12 +26,6 @@ router.get('/:id', (req, res) => {
         const posts = dbPostData.map(post => post.get({ plain: true }));
         res.render('regional', { posts, loggedIn: true });
 });
-=======
-router.get('/', (req, res) => {
-    
-
-    res.render('regional');
->>>>>>> 6bf743c7afd8feafd050e45fe6c4c5c8e2c4e0dc
 });
 
 module.exports = router;
